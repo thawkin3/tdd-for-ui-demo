f026b945ac3de07cb2a27cea1362a8f6743b11fd
Tyler Hawkins <tylerhawkins@workfront.com>
2021-01-15 13:21:57 -0700

    test: updates app page header test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,9 @@
 import { render, screen } from '@testing-library/react'
 import { App } from './App'
 
-test('renders learn react link', () => {
-  render(<App />)
-  expect(screen.getByText('TDD for Building UIs Demo')).toBeInTheDocument()
+describe('App', () => {
+  it('renders the page header', () => {
+    render(<App />)
+    expect(screen.getByText('TDD for Building UIs Demo')).toBeInTheDocument()
+  })
 })
