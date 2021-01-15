1fb97c0ada5361871ed5273e9a053d069dcfc6c4
Tyler Hawkins <tylerhawkins@workfront.com>
2021-01-15 16:19:24 -0700

    chore: sets project to initial state ready to begin the demo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css'
-import { RequestForm } from './RequestForm'
 
 export const App = () => {
   return (
     <main>
       <h1>TDD for Building UIs Demo</h1>
-      <RequestForm />
     </main>
   )
 }
