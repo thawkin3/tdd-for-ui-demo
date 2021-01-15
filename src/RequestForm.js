73842e47cc2f1ea900da996429d7f582270e170b
Tyler Hawkins <tylerhawkins@workfront.com>
2021-01-15 13:37:24 -0700

    feat: renders a first name input and label (passes)

--- src/RequestForm.js
+++ src/RequestForm.js
@@ -1,3 +1,8 @@
 import React from 'react'
 
-export const RequestForm = () => <form></form>
+export const RequestForm = () => (
+  <form>
+    <label htmlFor="firstName">First Name</label>
+    <input id="firstName" />
+  </form>
+)
